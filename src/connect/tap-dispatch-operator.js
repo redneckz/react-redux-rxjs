103f4968a8555526a2e713a629e758a7cda87b0b
redneckz <aalexandrovv@gmail.com>
2017-08-24 13:11:18 +0300

    Make dispatch operator smart

--- src/connect/tap-dispatch-operator.js
+++ src/connect/tap-dispatch-operator.js
@@ -1,19 +1,33 @@
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
 import isFunction from 'lodash/isFunction';
+import isPlainObject from 'lodash/isPlainObject';
+import cond from 'lodash/cond';
 import identity from 'lodash/identity';
+
+const adjustValueToAction = cond([
+    // Common case (Observable of actions)
+    [isPlainObject, identity],
+    // #2 Make dispatch operator smart
+    [isFunction, actionCreator => actionCreator()]
+]);
 
 export function tapDispatchOperator(dispatch) {
     return function tapDispatch(
-        actionCreator = identity,
+        actionCreator,
         errorActionCreator,
         completedActionCreator
     ) {
         if (this && isFunction(this.do)) {
-            return this.do(
-                arg => dispatch(actionCreator(arg)),
-                errorActionCreator && (err => dispatch(errorActionCreator(err))),
-                completedActionCreator && (() => dispatch(completedActionCreator()))
-            );
+            return this
+                .map(actionCreator || adjustValueToAction || identity)
+                .filter(Boolean)
+                .do(
+                    dispatch,
+                    errorActionCreator && (err => dispatch(errorActionCreator(err))),
+                    completedActionCreator && (() => dispatch(completedActionCreator()))
+                );
         }
         const action = actionCreator;
         return dispatch(action);
---
+++ src/connect/tap-dispatch-operator.spec.js
@@ -0,0 +1,62 @@
+import identity from 'lodash/identity';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/map';
+import {tapDispatchOperator} from './tap-dispatch-operator';
+
+describe('tapDispatchOperator', () => {
+    const DO_FOO = 'DO_FOO';
+    const doFoo = payload => ({type: DO_FOO, payload});
+
+    let originalDispatch;
+    let dispatch;
+    beforeEach(() => {
+        originalDispatch = jest.fn(identity);
+        dispatch = tapDispatchOperator(originalDispatch);
+    });
+
+    describe('out of context works as original [dispatch]', () => {
+        it('should invoke original [dispatch] with provided action', () => {
+            dispatch(doFoo(123));
+            expect(originalDispatch).toHaveBeenCalledTimes(1);
+            expect(originalDispatch).toHaveBeenCalledWith(doFoo(123));
+        });
+
+        it('should return provided action', () => {
+            expect(dispatch(doFoo(123))).toEqual(doFoo(123));
+        });
+    });
+
+    describe('as Observable operator', () => {
+        let foo$;
+        beforeEach(() => {
+            foo$ = Observable.of(123);
+        });
+
+        it('should dispatch values from Observable as actions', (done) => {
+            foo$.map(doFoo)::dispatch().subscribe(() => {
+                expect(originalDispatch).toHaveBeenCalledTimes(1);
+                expect(originalDispatch).toHaveBeenCalledWith(doFoo(123));
+                done();
+            });
+        });
+
+        it('should dispatch values from Observable as actions using provided [actionCreator]', (done) => {
+            foo$::dispatch(doFoo).subscribe(() => {
+                expect(originalDispatch).toHaveBeenCalledTimes(1);
+                expect(originalDispatch).toHaveBeenCalledWith(doFoo(123));
+                done();
+            });
+        });
+
+        it('should support Observable of action creators invoking each before dispatching', (done) => {
+            Observable.of(() => doFoo(123))
+                ::dispatch()
+                .subscribe(() => {
+                    expect(originalDispatch).toHaveBeenCalledTimes(1);
+                    expect(originalDispatch).toHaveBeenCalledWith(doFoo(123));
+                    done();
+                });
+        });
+    });
+});
